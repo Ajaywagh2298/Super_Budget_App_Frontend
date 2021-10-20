db9f6d0fb8078c3ba8c0b96c974cc1bccc8baa36
kamleshdugade <kamleesh.dugade20@vit.edu>
2021-10-21 00:07:17 +0530

    commit AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
 import {MatTableModule} from "@angular/material/table";
 import {MatButtonModule} from "@angular/material/button";
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerLoginComponent } from './customer-login/customer-login.component';
+import { HeaderCustomerComponent } from './header-customer/header-customer.component';
+import { SidenavCustomerComponent } from './sidenav-customer/sidenav-customer.component';
+import { HomeCustomerComponent } from './home-customer/home-customer.component';
+import { DashboardCustomerComponent } from './dashboard-customer/dashboard-customer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustomerLoginComponent,
+    HeaderCustomerComponent,
+    SidenavCustomerComponent,
+    HomeCustomerComponent,
+    DashboardCustomerComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
