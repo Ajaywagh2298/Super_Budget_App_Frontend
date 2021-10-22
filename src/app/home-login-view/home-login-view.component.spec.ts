668ee52a20dc248157462724389f05fd0ec6d2d4
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2021-10-22 23:31:08 +0530

    Commit

---
+++ src/app/home-login-view/home-login-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeLoginViewComponent } from './home-login-view.component';
+
+describe('HomeLoginViewComponent', () => {
+  let component: HomeLoginViewComponent;
+  let fixture: ComponentFixture<HomeLoginViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeLoginViewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeLoginViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/home-login-view/home-login-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-login-view',
+  templateUrl: './home-login-view.component.html',
+  styleUrls: ['./home-login-view.component.css']
+})
+export class HomeLoginViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
