2fd754a8516e15f2d6cb78ef4a54ff34ee6a878d
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2021-12-29 23:31:55 +0530

    commit "user details are edit"

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,16 +1,51 @@
-const express = require('express');
-
-
+/*const express = require('express');
 // create new express app and save it as "app"
 const app = express();
-
 // server configuration
-const PORT = 8080;
+const PORT = 8081;
 
 app.get('/', (req, res) => {
-  res.send("hello server is run !")
+  res.send('/dashboard')
 });
 // make the server listen to requests
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
+}); */
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "super-session",
+    secret: "COOKIE_SECRET", // should use as secret environment variable
+    httpOnly: true
+  })
+);
+
+// simple route
+app.get("/", (req, res) => {
+  //res.json({ message: "Welcome to the Server,<h1>localhost:${PORT}</h1>" });
+  res.send(`Welcome to the Server,<h1>localhost:${PORT}</h1>`);
 });
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
